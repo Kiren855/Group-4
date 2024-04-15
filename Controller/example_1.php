89392e89cb5d68a55bab7b05d277074edd6a0d88
tranngoclamm <124114520+tranngoclamm@users.noreply.github.com>
2024-04-15 17:12:36 +0000

    Add Example 1

---
+++ Controller/example_1.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Basic PHP</title>
+</head>
+<body>
+    <p>Văn bản HTML.</p>
+    <?php 
+        echo '<p>Văn bản PHP</p>';
+    ?>
+    <p>Văn bản HTML khác.</p>
+</body>
+</html>
