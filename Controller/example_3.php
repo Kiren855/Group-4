b4879e68b40853f1fdf5847ccd7401bb42304b1e
your name <21010291@st.phenikaa-uni.edu.vn>
2024-04-18 23:27:18 +0700

    Add Example_3

---
+++ Controller/example_3.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Basic PHP</title>
+</head>
+<body>
+    <?='<p>Some text to output</p>'?>
+    
+    <?php 
+        echo '<p>Some text to output</p>';
+    ?>
+</body>
+</html>
