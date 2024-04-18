7f5f974d60ef28e2dd00984471f9f30bd2eba4ee
your name <21010291@st.phenikaa-uni.edu.vn>
2024-04-18 23:24:30 +0700

    Add Example_2

---
+++ Controller/example_2.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Basic PHP</title>
+</head>
+<body>
+    <p>Văn bản HTML.</p>
+    <?php 
+        echo '<p>Khối dữ liệu PHP 1. </p>';
+    ?>
+    <p>Dữ liệu HTML, <?php echo 'Dữ liệu PHP 2'; ?></p>
+    <?php echo '<b>'; ?>
+        Một ví dụ kết hợp HTML và PHP
+    <?php echo '</b>'; ?>
+</body>
+</html>
