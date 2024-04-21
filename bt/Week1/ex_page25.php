fa78b4b9eeab7f15b0976e00d46e66949f320307
your name <21010291@st.phenikaa-uni.edu.vn>
2024-04-21 17:15:33 +0700

    example print page 25

---
+++ bt/Week1/ex_page25.php
@@ -0,0 +1,11 @@
+<?php
+    print("<p>I love PHP1.</p>");
+?>
+<?php
+    $WLang = "<p>I love PHP1.</p>";
+    print $WLang;
+?>
+<?php
+    print("<p>I love    
+                    PHP3.</p>");
+?>
